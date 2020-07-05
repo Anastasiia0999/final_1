d923afa148c300e12db689061d58011a077f411f
Anastasia_G <anastasiagordienko99@gmail.com>
2020-07-05 20:33:25 +0300

    comment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-/*import { FormController } from "./FormController.js";*/
-import { NewPostController } from "./NewPostController.js";
-/*import { Publisher } from "./helpers/Publisher.js"
 
-const publisher = new Publisher();*/
+import { NewPostController } from "./NewPostController.js";
+
+
 const newPostController = new NewPostController();
-/*const cardController = new CardController(publisher.methods);*/
+
